8c830214ab0d3e7fdaa7590788c997fb89aa47e7
Jorge Sánchez <47304153+ZeikkuSSJ7@users.noreply.github.com>
2022-03-29 16:13:20 +0200

    Pacman: inicio

---
+++ Pacman/Intermediate/Build/Win64/UE4Editor/Inc/Pacman/PacmanGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef PACMAN_PacmanGameModeBase_generated_h
+#error "PacmanGameModeBase.generated.h already included, missing '#pragma once' in PacmanGameModeBase.h"
+#endif
+#define PACMAN_PacmanGameModeBase_generated_h
+
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_SPARSE_DATA
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_RPC_WRAPPERS
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAPacmanGameModeBase(); \
+	friend struct Z_Construct_UClass_APacmanGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APacmanGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Pacman"), NO_API) \
+	DECLARE_SERIALIZER(APacmanGameModeBase)
+
+
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAPacmanGameModeBase(); \
+	friend struct Z_Construct_UClass_APacmanGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(APacmanGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/Pacman"), NO_API) \
+	DECLARE_SERIALIZER(APacmanGameModeBase)
+
+
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APacmanGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APacmanGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APacmanGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APacmanGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APacmanGameModeBase(APacmanGameModeBase&&); \
+	NO_API APacmanGameModeBase(const APacmanGameModeBase&); \
+public:
+
+
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API APacmanGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API APacmanGameModeBase(APacmanGameModeBase&&); \
+	NO_API APacmanGameModeBase(const APacmanGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, APacmanGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(APacmanGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(APacmanGameModeBase)
+
+
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_12_PROLOG
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_SPARSE_DATA \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_RPC_WRAPPERS \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_INCLASS \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Pacman_Source_Pacman_PacmanGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_SPARSE_DATA \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	Pacman_Source_Pacman_PacmanGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> PACMAN_API UClass* StaticClass<class APacmanGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Pacman_Source_Pacman_PacmanGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
