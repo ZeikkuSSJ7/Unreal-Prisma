faa816e798bfcdcc0b294e5f41992a0185818b8f
Jorge Sánchez <47304153+ZeikkuSSJ7@users.noreply.github.com>
2022-05-23 13:38:48 +0200

    Prueba Videos

---
+++ PruebaVideoStreaming/Source/PruebaVideoStreaming/PruebaVideoStreamingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PruebaVideoStreamingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRUEBAVIDEOSTREAMING_API APruebaVideoStreamingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
