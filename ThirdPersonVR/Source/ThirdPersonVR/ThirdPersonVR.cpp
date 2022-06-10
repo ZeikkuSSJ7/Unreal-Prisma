f1f1ea3d85dabe2109afd82b9b3053e8ff50c00b
Jorge Sánchez <47304153+ZeikkuSSJ7@users.noreply.github.com>
2022-06-10 13:20:14 +0200

    oh no

--- ThirdPersonVR/Source/ThirdPersonVR/ThirdPersonVR.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ThirdPersonVR.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdPersonVR, "ThirdPersonVR" );
- 
