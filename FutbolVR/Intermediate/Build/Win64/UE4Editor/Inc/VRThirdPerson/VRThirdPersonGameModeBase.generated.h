eb025b30a16417f4ca323782ee7c9ce1acbea5dd
Jorge Sánchez <47304153+ZeikkuSSJ7@users.noreply.github.com>
2022-06-10 15:49:07 +0200

    Commit final (creo)

---
+++ FutbolVR/Intermediate/Build/Win64/UE4Editor/Inc/VRThirdPerson/VRThirdPersonGameModeBase.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VRTHIRDPERSON_VRThirdPersonGameModeBase_generated_h
+#error "VRThirdPersonGameModeBase.generated.h already included, missing '#pragma once' in VRThirdPersonGameModeBase.h"
+#endif
+#define VRTHIRDPERSON_VRThirdPersonGameModeBase_generated_h
+
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_SPARSE_DATA
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_RPC_WRAPPERS
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRThirdPersonGameModeBase(); \
+	friend struct Z_Construct_UClass_AVRThirdPersonGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AVRThirdPersonGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRThirdPerson"), NO_API) \
+	DECLARE_SERIALIZER(AVRThirdPersonGameModeBase)
+
+
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRThirdPersonGameModeBase(); \
+	friend struct Z_Construct_UClass_AVRThirdPersonGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AVRThirdPersonGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/VRThirdPerson"), NO_API) \
+	DECLARE_SERIALIZER(AVRThirdPersonGameModeBase)
+
+
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRThirdPersonGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRThirdPersonGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRThirdPersonGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRThirdPersonGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRThirdPersonGameModeBase(AVRThirdPersonGameModeBase&&); \
+	NO_API AVRThirdPersonGameModeBase(const AVRThirdPersonGameModeBase&); \
+public:
+
+
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRThirdPersonGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRThirdPersonGameModeBase(AVRThirdPersonGameModeBase&&); \
+	NO_API AVRThirdPersonGameModeBase(const AVRThirdPersonGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRThirdPersonGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRThirdPersonGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRThirdPersonGameModeBase)
+
+
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_12_PROLOG
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_SPARSE_DATA \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_RPC_WRAPPERS \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_INCLASS \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_SPARSE_DATA \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> VRTHIRDPERSON_API UClass* StaticClass<class AVRThirdPersonGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FutbolVR_Source_VRThirdPerson_VRThirdPersonGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
